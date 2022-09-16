080b1cc40ab387e8bffde2737b840ccb190f3b0a
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-16 12:58:48 +0800

    [UPDATE] / Add .env port

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080")
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
 })
 
 // use routes
