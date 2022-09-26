56162ca59876a9ceefdd5c3b52d9288d0e1a5d77
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-26 18:56:45 +0800

    [QOL] / Add alternative ports

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 8090, () => {
+    console.log(`Server running on port ${process.env.PORT || 8090}`)
 })
 
 // use routes
